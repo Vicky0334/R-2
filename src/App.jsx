64a4038d6f51bc5406b4ba9af24e4104d5377717
Vicky Dhote <dhoteji034@gmail.com>
2024-05-21 15:14:04 +0530

    some update

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import Home from "./components/Home";
 import Users from "./components/Users";
 import About from "./components/About";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import { Route, Routes} from 'react-router-dom';
 const App = () => {
   return (
-      <div className="w-[80%] ">
+      <div className="w-[80%] p-10 m-auto">
           <Nav />
-
-          <Router>
+          
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/users" element={<Users />} />
               <Route path="/about" element={<About />} />
               
-          </Routes>
-          </Router>
+          </Routes>          
       </div>
   );
 };
